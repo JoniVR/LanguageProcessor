54963f154c4ad147a678b64b6f9ff31b96012723
Toon Van Deuren <toon.vandeuren@student.kdg.be>
2019-10-09 11:15:21 +0200

    Hello World! 👋👋👋

--- src/view/NGramsView.scala
+++ src/view/NGramsView.scala
@@ -1,9 +1,13 @@
 package view
 
+import javafx.scene.control.Label
 import javafx.scene.layout.GridPane
 import presenter.NGramsPresenter
 
 class NGramsView(NGramsPresenter: NGramsPresenter) extends GridPane
 {
   setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, DarkSlateGray, Black);")
+
+  val label = new Label("Hello World!")
+  this.getChildren.add(label);
 }
