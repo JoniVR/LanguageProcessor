f2174f429b61b8578dfd5c9bdc7887ad168ab3e1
Toon Van Deuren <toon.vandeuren@student.kdg.be>
2019-10-09 12:09:19 +0200

    Adding style

--- src/view/NGramsView.scala
+++ src/view/NGramsView.scala
@@ -1,13 +1,23 @@
 package view
 
+import java.net.URL
+
 import javafx.scene.control.Label
 import javafx.scene.layout.GridPane
 import presenter.NGramsPresenter
 
 class NGramsView(NGramsPresenter: NGramsPresenter) extends GridPane
 {
-  setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, DarkSlateGray, Black);")
+  // Adding css stylesheet, found in the resources folder
+  val css : String = this.getClass.getClassLoader.getResource("style/style.css").toExternalForm
+  getStylesheets.add(css)
+
+  // Setting width and height
+  this.setMaxWidth(800)
+  this.setMaxHeight(600)
 
   val label = new Label("Hello World!")
-  this.getChildren.add(label);
+  label.setId("hello")
+
+  this.getChildren.add(label)
 }
